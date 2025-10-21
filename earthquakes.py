fcfa827d36bcff894bea2002cbab09e5d320aa95
clara-lw <ucaplat@ucl.ac.uk>
2025-10-21 11:23:53 +0100

    Update earthquakes.py

--- earthquakes.py
+++ earthquakes.py
@@ -59,10 +59,3 @@ data = get_data()
 print(f"Loaded {count_earthquakes(data)}")
 max_magnitude, max_location = get_maximum(data)
 print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
-
-
-# With all the above functions defined, we can now call them and get the result
-data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
-max_magnitude, max_location = get_maximum(data)
-print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
